0715113f6edcff280d7e9239d4be5585e841ac18
rigizer <rigizer@gmail.com>
2023-04-14 17:31:31 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 132 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.py
@@ -0,0 +1,6 @@
+n, m = map(int, input().split())
+box = [i + 1 for i in range(n)]
+for _ in range(m):
+    i, j, k = map(int, input().split())
+    box = box[:i-1] + box[k-1:j] + box[i-1:k-1] + box[j:]
+print(*box)
