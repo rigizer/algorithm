f2592896f19e8b19e53ba1bed25830aba3a3d171
rigizer <rigizer@gmail.com>
2023-04-20 21:18:48 +0900

    [Gold III] Title: 웜홀, Time: 424 ms, Memory: 117808 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -10,12 +10,15 @@ def bellman_ford(n, graph, distance):
         # 반복시 모든 간선 확인
         for v in range(1, n + 1):
             for next_v, next_g in graph[v]:
+                # 다음 노드로 이동하는 거리가 최단거리로 갱신 가능한 경우
                 if distance[next_v] > distance[v] + next_g:
                     distance[next_v] = distance[v] + next_g
 
+                    # 음의 사이클이 발생한 경우
                     if i == n - 1:
                         return False
     
+    # 출발 위치로 아무런 이상없이 온 경우
     return True
 
 tc = int(input())
@@ -25,7 +28,9 @@ for _ in range(tc):
 
     # 각 지점은 1부터 시작
     graph = [[] for _ in range(n + 1)]
-    distance = [1e9] * (n + 1)
+
+    # 최단거리 초기화
+    distance = [2e9] * (n + 1)
 
     # 도로의 간선정보 입력 (도로에는 방향이 없음, 양방향)
     for i in range(m):
