afbd02cd5985e551ca91984cc254e47bab91e0c2
rigizer <rigizer@gmail.com>
2023-04-22 22:45:44 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,32 @@
+while True:
+    try:
+        s = input()
+    except:
+        break
+
+    if s == 'CU':
+        print('see you')
+    elif s == ':-)':
+        print('I’m happy')
+    elif s == ':-(':
+        print('I’m unhappy')
+    elif s == ';-)':
+        print('wink')
+    elif s == ':-P':
+        print('stick out my tongue')
+    elif s == '(~.~)':
+        print('sleepy')
+    elif s == 'TA':
+        print('totally awesome')
+    elif s == 'CCC':
+        print('Canadian Computing Competition')
+    elif s == 'CUZ':
+        print('because')
+    elif s == 'TY':
+        print('thank-you')
+    elif s == 'YW':
+        print('you’re welcome')
+    elif s == 'TTYL':
+        print('talk to you later')
+    else:
+        print(s)
