9b6507c7e399a86a5ff3d0533dde5093bbb9f3d6
rigizer <rigizer@gmail.com>
2023-04-28 00:59:35 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,15 +1,3 @@
 def solution(skill, skill_trees):
-    answer = 0
-    
-    for skill_tree in skill_trees:
-        checked = []
-        for s in skill_tree:
-            if s in skill:
-                idx = skill.index(s)
-                if idx == 0 or skill[idx - 1] in checked:
-                    checked.append(s)
-                else:
-                    break
-        else:
-            answer += 1
+    answer = -1
     return answer
