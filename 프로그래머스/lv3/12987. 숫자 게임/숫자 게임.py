c6092ef6eae93b2a0ba0652b369c76da3f75f011
rigizer <rigizer@gmail.com>
2023-05-04 20:20:28 +0900

    [level 3] Title: 숫자 게임, Time: 56.96 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -1,17 +1,3 @@
 def solution(A, B):
-    answer = 0
-
-    A.sort()
-    B.sort()
-
-    a_idx = 0
-    b_idx = 0
-    while b_idx < len(B):
-        if B[b_idx] > A[a_idx]:
-            answer += 1
-            a_idx += 1
-            b_idx += 1
-        else:
-            b_idx += 1
-
+    answer = -1
     return answer
