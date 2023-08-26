cec722103aa72705aff1b698350b1c6b4ef08ff0
rigizer <rigizer@gmail.com>
2023-08-26 16:00:25 +0900

    [Bronze IV] Title: H4x0r, Time: 248 ms, Memory: 173148 KB -BaekjoonHub

---
+++ 백준/Bronze/26768. H4x0r/H4x0r.py
@@ -0,0 +1,5 @@
+replace_dict = {
+    'a': '4', 'e': '3', 'i': '1', 'o': '0', 's': '5'
+}
+string = input()
+print(''.join([replace_dict[word] if word in replace_dict else word for word in list(string)]))
