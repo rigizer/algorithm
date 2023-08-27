a18ec81f682d44d9e029dd9004d44ff6b624269d
rigizer <rigizer@gmail.com>
2023-08-27 18:42:28 +0900

    [Bronze IV] Title: 방학 숙제, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.py
@@ -0,0 +1,16 @@
+l = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+result = 0
+for day in range(1, l + 1):
+    a -= c
+    b -= d
+
+    if a <= 0 and b <= 0:
+        result = l - day
+        break
+
+print(result)
