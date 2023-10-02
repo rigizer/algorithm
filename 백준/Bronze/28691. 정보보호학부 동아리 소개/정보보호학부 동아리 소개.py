e38429a0cfc2b724992de99bf799ab15d2331845
Jaeyong Han <rigizer@gmail.com>
2023-10-02 19:10:32 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+club = input()
+if club == 'M':
+    print('MatKor')
+elif club == 'W':
+    print('WiCys')
+elif club == 'C':
+    print('CyKor')
+elif club == 'A':
+    print('AlKor')
+else:
+    print('$clear')
