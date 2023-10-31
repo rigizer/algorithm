8f25fa6a903ec0936acf7147aeb5042eced4e681
Jaeyong Han <rigizer@gmail.com>
2023-11-01 08:31:56 +0900

    [Bronze III] Title: 사분면, Time: 120 ms, Memory: 110272 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.py
@@ -0,0 +1,26 @@
+n = int(input())
+axis = 0
+q1 = 0
+q2 = 0
+q3 = 0
+q4 = 0
+
+for _ in range(n):
+  x, y = map(int, input().split())
+  
+  if x == 0 or y == 0:
+    axis += 1
+  elif x > 0 and y > 0:
+    q1 += 1
+  elif x < 0 and y > 0:
+    q2 += 1
+  elif x < 0 and y < 0:
+    q3 += 1
+  else:
+    q4 += 1
+
+print('Q1:', q1)
+print('Q2:', q2)
+print('Q3:', q3)
+print('Q4:', q4)
+print('AXIS:', axis)
