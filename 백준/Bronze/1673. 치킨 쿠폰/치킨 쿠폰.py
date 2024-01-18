85323a66c4f8da4fa8b08463e24ec0baf0eed8f9
Jaeyong Han <rigizer@gmail.com>
2024-01-18 09:21:27 +0900

    [Bronze II] Title: 치킨 쿠폰, Time: 124 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/1673. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,10 @@
+try:
+    while True:
+        n, k = map(int, input().rstrip().split())
+        chicken = n + (n // k)
+        while n >= k:
+            n = (n // k) + (n % k) 
+            chicken += n // k
+        print(chicken)
+except:
+    exit()
