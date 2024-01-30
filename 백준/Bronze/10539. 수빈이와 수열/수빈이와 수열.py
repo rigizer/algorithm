141b76d81400367b802e07a36c567bd2bff00b74
Jaeyong Han <rigizer@gmail.com>
2024-01-30 09:11:44 +0900

    [Bronze II] Title: 수빈이와 수열, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/10539. 수빈이와 수열/수빈이와 수열.py
@@ -0,0 +1,8 @@
+n = int(input())
+array = list(map(int, input().split()))
+
+result = [array[0]]
+for i in range(1, n):
+    result.append(array[i] * (i+1) - sum(result))
+for i in result:
+    print(i, end=' ')
