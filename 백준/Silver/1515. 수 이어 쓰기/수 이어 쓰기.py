24cc42041895bf50ad5773062778d2452a6975e0
Jaeyong Han <rigizer@gmail.com>
2024-03-09 22:44:25 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 116 ms, Memory: 110272 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -0,0 +1,11 @@
+nums = input()
+n = 0
+idx = 0
+while True:
+    n += 1
+    for s in str(n):
+        if nums[idx] == s:
+            idx += 1
+            if idx >= len(nums):
+                print(n)
+                exit()
