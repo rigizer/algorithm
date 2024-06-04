7a4b7cc1d877a6030a30ac370e7c60ebc394d746
Jaeyong Han <rigizer@gmail.com>
2024-06-04 11:35:19 +0900

    [Silver III] Title: 달팽이, Time: 276 ms, Memory: 120584 KB -BaekjoonHub

---
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -0,0 +1,53 @@
+n, m = int(input()), int(input())
+snail = [[0] * n for _ in range(n)]
+
+x = (n - 1) // 2
+y = (n - 1) // 2
+snail[x][y] = 1
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+i = 2
+start = 3
+
+while x != 0 or y != 0:
+    while i <= start * start:
+        if x == y == (n - 1) // 2:
+            cnt_up, cnt_down, cnt_left, cnt_right = start, start - 1, start - 1, start - 2
+            x += dx[0]
+            y += dy[0]
+            cnt_up -= 1
+
+        elif cnt_right > 0:
+            x += dx[3]
+            y += dy[3]
+            cnt_right -= 1
+
+        elif cnt_down > 0:
+            x += dx[1]
+            y += dy[1]
+            cnt_down -= 1
+
+        elif cnt_left > 0:
+            x += dx[2]
+            y += dy[2]
+            cnt_left -= 1
+
+        elif cnt_up > 0:
+            x += dx[0]
+            y += dy[0]
+            cnt_up -= 1
+
+        snail[x][y] = i
+        i += 1
+
+    n -= 2
+    start += 2
+
+for j in range(len(snail)):
+    print(*snail[j])
+    if m in snail[j]:
+        mx = j + 1
+        my = snail[j].index(m) + 1
+print(mx, my)
