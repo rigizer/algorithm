79c12c079f7e24400cc62ca112f31dd8bff37cca
Jaeyong Han <rigizer@gmail.com>
2024-06-06 16:46:50 +0900

    [Silver V] Title: 돌 게임 2, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+if n == 1:
+    print('CY')
+elif n == 2:
+    print('SK')
+elif n == 3:
+    print('CY')
+else:
+    dp = [-1] * (n + 1)
+    dp[1] = 'CY'
+    dp[2] = 'SK'
+    dp[3] = 'CY'
+
+    for i in range(4, n + 1):
+        if dp[i - 1] == 'CY' or dp[i - 3] == 'CY':
+            dp[i] = 'SK'
+        else:
+            dp[i] = 'CY'
+    print(dp[n])
