3be7e7458555fcd48236161e68ee404f375e9cee
Jaeyong Han <rigizer@gmail.com>
2024-06-15 09:46:55 +0900

    [Bronze III] Title: 주식 투자, Time: 208 ms, Memory: 110608 KB -BaekjoonHub

---
+++ 백준/Bronze/13416. 주식 투자/주식 투자.py
@@ -0,0 +1,7 @@
+for _ in range(int(input())):
+    mymax = 0
+    for i in range(int(input())):
+        tmp = list(map(int, input().split()))
+        if max(tmp) >= 0:
+            mymax += max(tmp)
+    print(mymax)
