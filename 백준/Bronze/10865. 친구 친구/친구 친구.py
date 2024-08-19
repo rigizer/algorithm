f2f3fd6f26c3ca5819a40e438bcec2e9fc5e41c3
Jaeyong Han <rigizer@gmail.com>
2024-08-19 22:41:17 +0900

    [Bronze III] Title: 친구 친구, Time: 480 ms, Memory: 112096 KB -BaekjoonHub

---
+++ 백준/Bronze/10865. 친구 친구/친구 친구.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+cnt = [0] * (n + 1)
+
+for i in range(m):
+    a, b = map(int, input().split())
+    cnt[a] += 1
+    cnt[b] += 1
+
+for i in range(1, n + 1):
+    print(cnt[i])
