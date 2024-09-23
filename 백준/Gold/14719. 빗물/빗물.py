4849009dc49e46eebaf6a3141c94a6bb0b997288
Jaeyong Han <rigizer@gmail.com>
2024-09-23 20:08:11 +0900

    [Gold V] Title: 빗물, Time: 100 ms, Memory: 110592 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -0,0 +1,32 @@
+def calc(h, w, block):
+    result = 0
+
+    for i in range(1, h + 1):
+        chk = 0
+        start = False
+        end = False
+
+        for j in range(w):
+            if start == False:
+                if block[j] >= i:
+                    start = True
+            elif start == True:
+                if block[j] < i:
+                    chk += 1
+                else:
+                    if chk == 0:
+                        continue
+
+                    end = True
+                    result += chk
+                    chk = 0
+                    start = True
+                    end = False
+
+    return result
+
+h, w = map(int, input().split())
+block = list(map(int, input().split()))
+
+result = calc(h, w, block)
+print(result)
