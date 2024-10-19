3b5990308a67143fd45c0f128a87e7625287be2c
Jaeyong Han <rigizer@gmail.com>
2024-10-19 18:47:27 +0900

    [Bronze II] Title: 중복을 없애자, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/4592. 중복을 없애자/중복을 없애자.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        tmp = list(map(int, input().split()))
+        ans = []
+        ans.append(tmp[1])
+        for i in range(2, len(tmp)):
+            if ans[-1] != tmp[i]:
+                ans.append(tmp[i])
+        for i in range(len(ans)):
+            print(ans[i], end=' ')
+        print('$')
+    except:
+        exit()
