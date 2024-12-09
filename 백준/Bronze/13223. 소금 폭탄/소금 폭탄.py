eeb7052ddda055a621bbcfcd8b49d1b6c39201f9
Jaeyong Han <rigizer@gmail.com>
2024-12-09 18:07:57 +0900

    [Bronze III] Title: 소금 폭탄, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.py
@@ -0,0 +1,16 @@
+now = list(map(int, input().split(':')))
+salt= list(map(int, input().split(':')))
+
+now_time = now[0] * 3600 + now[1] * 60 + now[2]
+salt_time = salt[0] * 3600 + salt[1] * 60 + salt[2]
+
+if now_time >= salt_time:
+    salt_time += 24 * 3600
+
+result = salt_time - now_time
+
+hh = str(result // 3600).zfill(2)
+mm = str((result % 3600) // 60).zfill(2)
+ss = str(result % 60).zfill(2)
+
+print('{}:{}:{}'.format(hh, mm, ss))
