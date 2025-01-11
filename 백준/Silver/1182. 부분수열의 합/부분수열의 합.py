752ed8da37d57d8e7f06f9180655c2bb5e98b43b
Jaeyong Han <rigizer@gmail.com>
2025-01-11 18:51:29 +0900

    [Silver II] Title: 부분수열의 합, Time: 440 ms, Memory: 117124 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,17 @@
+n, s = map(int, input().split())
+num = list(map(int, input().split()))
+cnt = 0
+ans = []
+
+def solve(start):
+    global cnt
+    if sum(ans) == s and len(ans) > 0:
+        cnt += 1
+
+    for i in range(start, n):
+        ans.append(num[i])
+        solve(i + 1)
+        ans.pop()
+
+solve(0)
+print(cnt)
