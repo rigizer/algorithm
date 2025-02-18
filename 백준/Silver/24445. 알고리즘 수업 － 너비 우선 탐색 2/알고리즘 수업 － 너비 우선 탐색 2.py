bf49bb65595330f33b59936453e6865dd39a31d5
Jaeyong Han <rigizer@gmail.com>
2025-02-18 21:20:30 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 412 ms, Memory: 143748 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+graph = [sorted(r, reverse=True) for r in graph]
+
+result = [0 for _ in range(n + 1)]
+count = 0
+
+queue = deque()
+visited = [False] * (n + 1)
+
+queue.append(r)
+visited[r] = True
+count += 1
+result[r] = count
+
+while queue:
+    node = queue.popleft()
+
+    for i in graph[node]:
+        if visited[i] == False:
+            queue.append(i)
+            visited[i] = True
+            count += 1
+            result[i] = count
+
+print('\n'.join(map(str, [result[i] for i in range(1, n + 1)])))
