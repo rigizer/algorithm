434019ab0a4082d6c0a23878bb4848d60769227f
Jaeyong Han <rigizer@gmail.com>
2025-02-19 21:35:47 +0900

    [Silver III] Title: 세 번 이내에 사과를 먹자, Time: 112 ms, Memory: 112308 KB -BaekjoonHub

--- 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.py
+++ 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.py
@@ -1,37 +1,33 @@
 from collections import deque
 
-dy = [0, 1, 0, -1]
-dx = [-1, 0, 1, 0]
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
 
 board = [list(map(int, input().split())) for _ in range(5)]
 r, c = map(int, input().split())
+result = 0
 
-def bfs(r, c):
-    queue = deque()
-    queue.append((r, c, 0, 0, -1, -1))
+visited = [[False] * 5 for _ in range(5)]
+visited[r][c] = True
 
-    while queue:
-        y, x, t, a, by, bx = queue.popleft()
+def dfs(y, x, t, a):
+    if t <= 3:
+        if a >= 2:
+            print(1)
+            exit()
+        if t == 3:
+            return
 
-        if t <= 3:
-            if a >= 2:
-                return 1
-            if t == 3:
-                continue
-        
-        for d in range(4):
-            ny = y + dy[d]
-            nx = x + dx[d]
+    for d in range(4):
+        ny = y + dy[d]
+        nx = x + dx[d]
 
-            if 0 <= ny < 5 and 0 <= nx < 5:
-                if board[ny][nx] != -1:
-                    if (ny, nx) != (by, bx):
-                        if board[ny][nx] == 0:
-                            queue.append((ny, nx, t + 1, a, y, x))
-                        elif board[ny][nx] == 1:
-                            queue.append((ny, nx, t + 1, a + 1, y, x))
+        if 0 <= ny < 5 and 0 <= nx < 5:
+            if board[ny][nx] != -1:
+                if visited[ny][nx] == False:
+                    visited[ny][nx] = True
+                    dfs(ny, nx, t + 1, a + board[ny][nx])
+                    visited[ny][nx] = False
 
-    return 0
-
-result = bfs(r, c)
-print(result)
+dfs(r, c, 0, 0)
+print(0)
