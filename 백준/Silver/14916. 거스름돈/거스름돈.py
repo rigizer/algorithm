f46ecd84c88696eb5ea32fd0e7d3495d2406409c
Jaeyong Han <rigizer@gmail.com>
2025-03-17 18:19:35 +0900

    [Silver V] Title: 거스름돈, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,16 @@
+n = int(input())
+ans = 0
+while True:
+    if n % 5 == 0:
+        ans += n // 5
+        break
+    else:
+        n -= 2
+        ans += 1
+    if n < 0:
+        break
+
+if n < 0:
+    print(-1)
+else:
+    print(ans)
