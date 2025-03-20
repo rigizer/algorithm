5947a32770be07b4b26531a1d85496adb85aafd6
Jaeyong Han <rigizer@gmail.com>
2025-03-20 16:12:55 +0900

    [Silver V] Title: 영화감독 숌, Time: 524 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+n = int(input())
+idx = 0
+result = 0
+
+for i in range(666, 999999999):
+    if '666' in str(i):
+        idx += 1
+        if idx == n:
+            result = i
+            break
+
+print(result)
