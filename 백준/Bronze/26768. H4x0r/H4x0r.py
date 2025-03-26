cdaf6772fb92970957c5ac4cfd678c3ba8e8cbb4
Jaeyong Han <rigizer@gmail.com>
2025-03-26 16:08:11 +0900

    [Bronze IV] Title: H4x0r, Time: 212 ms, Memory: 168828 KB -BaekjoonHub

--- 백준/Bronze/26768. H4x0r/H4x0r.py
+++ 백준/Bronze/26768. H4x0r/H4x0r.py
@@ -1,5 +1,2 @@
-replace_dict = {
-    'a': '4', 'e': '3', 'i': '1', 'o': '0', 's': '5'
-}
-string = input()
-print(''.join([replace_dict[word] if word in replace_dict else word for word in list(string)]))
+replace_dict = { 'a': '4', 'e': '3', 'i': '1', 'o': '0', 's': '5' }
+print(''.join([replace_dict[word] if word in replace_dict else word for word in list(input())]))
