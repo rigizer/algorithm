a8d4240f2ed0b6ce7fc2a0838bc970ef473830e6
Jaeyong Han <rigizer@gmail.com>
2025-05-08 21:00:36 +0900

    [Bronze I] Title: Doubles, Time: 60 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Bronze/4641. Doubles/Doubles.java
+++ 백준/Bronze/4641. Doubles/Doubles.java
@@ -5,28 +5,25 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
+
         while (true) {
-            int result = 0;
             st = new StringTokenizer(br.readLine());
             List<Integer> list = new ArrayList<>();
+            Set<Integer> set = new HashSet<>();
+
             while (st.hasMoreTokens()) {
                 int n = Integer.parseInt(st.nextToken());
-                if (n != 0) {
-                    list.add(n);
-                }
+                if (n == 0) break;
+                list.add(n);
+                set.add(n);
             }
 
-            if (list.get(0) == -1) {
-                break;
-            }
+            if (list.size() == 1 && list.get(0) == -1) break;
 
-            int size = list.size();
-            for (int i = 0; i < size; i++) {
-                for (int j = i + 1; j < size; j++) {
-                    if (list.get(i) == list.get(j) * 2 || list.get(j) == list.get(i) * 2) {
-                        result++;
-                    }
+            int result = 0;
+            for (int num : list) {
+                if (set.contains(num * 2)) {
+                    result++;
                 }
             }
 
