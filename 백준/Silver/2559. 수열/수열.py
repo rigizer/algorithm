7c417468663861430ced05fdaed17729693edf1a
Jaeyong Han <rigizer@gmail.com>
2025-05-12 22:47:27 +0900

    [Silver III] Title: 수열, Time: 108 ms, Memory: 125100 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,9 @@
+n, k = map(int,input().split())
+a = list(map(int, input().split()))
+
+result = []
+result.append(sum(a[:k]))
+
+for i in range(n - k):
+    result.append(result[i] - a[i] + a[k+i])
+print(max(result))
