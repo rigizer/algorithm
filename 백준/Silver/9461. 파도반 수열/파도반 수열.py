a8e10591eb1fb1dbf6e7bfbe22577a2d94bd3af4
Jaeyong Han <rigizer@gmail.com>
2025-07-15 11:15:58 +0900

    [Silver III] Title: 파도반 수열, Time: 128 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,13 @@
+arr = [0, 1, 1, 1] + [0 for _ in range(97)]
+
+def func(x):
+  if arr[x]:
+    return arr[x]
+  else:
+    arr[x] = func(x - 2) + func(x - 3)
+    return arr[x]
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(func(n))
