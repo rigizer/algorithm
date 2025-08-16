c5c2ee3848bafb6832d0d52d1b9abb38772b3f12
Jaeyong Han <rigizer@gmail.com>
2025-08-16 10:27:52 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.55 ms, Memory: 9.48 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,23 @@
+def parse_date(date):
+    yy, mm, dd = map(int, date.split('.'))
+    return (yy * 12 * 28) + (mm * 28) + dd
+
+def solution(today, terms, privacies):
+    answer = []
+    
+    today_val = parse_date(today)
+    terms_val = dict()
+    
+    for term in terms:
+        key, val = map(str, term.split())
+        terms_val[key] = int(val) * 28
+    
+    for i, privacy in enumerate(privacies):
+        key, val = map(str, privacy.split())
+        p_val = parse_date(key) + terms_val.get(val)
+        print(today_val, p_val, today, key, p_val < today_val)
+        
+        if p_val <= today_val:
+            answer.append(i + 1)
+    
+    return answer
