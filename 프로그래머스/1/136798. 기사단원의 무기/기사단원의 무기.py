d806c96e62f6f87607b95ae395a54839c74fdf87
Jaeyong Han <rigizer@gmail.com>
2025-08-16 21:35:55 +0900

    [level 1] Title: 기사단원의 무기, Time: 943.53 ms, Memory: 9.48 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,16 @@
+def solution(number, limit, power):
+    answer = 0
+
+    for i in range(1, number + 1):
+        cnt = 0
+        for j in range(1, int(i ** 0.5) + 1):
+            if i % j == 0:
+                cnt += 1
+                if j != i // j:
+                    cnt += 1
+        if cnt > limit:
+            answer += power
+        else:
+            answer += cnt
+
+    return answer
