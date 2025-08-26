eb9c3ce37a242effbc64dfe0b643d706ea2f6ce6
Jaeyong Han <rigizer@gmail.com>
2025-08-26 21:31:31 +0900

    [Silver II] Title: 회문수, Time: 304 ms, Memory: 60604 KB -BaekjoonHub

---
+++ 백준/Silver/30446. 회문수/회문수.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+result = []
+odd = [str(i) for i in range(10)]
+even = [i+i for i in odd]
+
+result = odd + even
+temp1 = result[:]
+
+for _ in range(4):
+    temp2 = []
+    temp3 = []
+    for i in temp1:
+        for j in range(10):
+            x = str(j) + i + str(j)
+            temp2.append(x)
+            if j != 0:
+                temp3.append(x)
+    result.extend(temp3)
+    temp1 = temp2[:]
+
+result.remove('0')
+result.remove('00')
+
+answer = []
+for i in result:
+    x = int(i)
+    if x <= n:
+        answer.append(x)
+
+answer.sort()
+print(len(answer))
