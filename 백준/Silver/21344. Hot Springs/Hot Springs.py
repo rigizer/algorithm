2b3266d985ef738223b981df26ab7979198d5ef5
Jaeyong Han <rigizer@gmail.com>
2025-08-29 21:24:22 +0900

    [Silver II] Title: Hot Springs, Time: 108 ms, Memory: 43632 KB -BaekjoonHub

--- 백준/Silver/21344. Hot Springs/Hot Springs.py
+++ 백준/Silver/21344. Hot Springs/Hot Springs.py
@@ -1,19 +1,13 @@
 import sys
-input = lambda: sys.stdin.readline().rstrip()
-
-n = int(input())
-t = list(map(int, input().split()))
+input=lambda: sys.stdin.readline().rstrip()
+n=int(input())
+t=list(map(int,input().split()))
 t.sort()
-
-mid = (n - 1) // 2
-res = [t[mid]]
-
-for i in range(1, mid + 1):
-    if mid + i < n:
-        res.append(t[mid + i])
-    res.append(t[mid - i])
-
-if n % 2 == 0:
-    res.append(t[-1])
-
-print(' '.join(map(str, res)))
+mid=(n-1)//2
+res=[t[mid]]
+for i in range(1,mid+1):
+    if mid+i<n:
+        res.append(t[mid+i])
+    res.append(t[mid-i])
+if n%2==0: res.append(t[-1])
+print(' '.join(map(str,res)))
