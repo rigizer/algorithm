5b99fc919b7da3af34c5c6ac4cba42ac0eda072e
Jaeyong Han <rigizer@gmail.com>
2025-08-29 22:11:48 +0900

    [Bronze II] Title: 그릇, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/7567. 그릇/그릇.py
@@ -0,0 +1,13 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+dish = list(input())
+answer = 0
+for i in range(len(dish)):
+    if i == 0:
+        answer += 10
+    elif dish[i] == dish[i - 1]:
+        answer += 5
+    else:
+        answer += 10
+print(answer)
