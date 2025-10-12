7a9cfa5c18ec153d2eb3f7e0fbe62256697b5e4a
Jaeyong Han <rigizer@gmail.com>
2025-10-12 14:13:33 +0900

    [Silver V] Title: 크로스워드 퍼즐, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9626. 크로스워드 퍼즐/크로스워드 퍼즐.py
@@ -0,0 +1,24 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+m, n = map(int, input().split())
+u, l, r, d = map(int, input().split())
+puzzle = [list(input()) for _ in range(m)]
+
+board = [['#'] * (n + l + r) for _ in range(m + u + d)]
+
+for i in range(m + u + d):
+    for j in range(n + l + r):
+        if i % 2 == 0 and j % 2 == 1:
+            board[i][j] = '.'
+        elif i % 2 == 1 and j % 2 == 0:
+            board[i][j] = '.'
+
+for i in range(m):
+    for j in range(n):
+        board[i + u][j + l] = puzzle[i][j]
+
+for i in range(m + u + d):
+    for j in range(n + l + r):
+        print(board[i][j], end='')
+    print()
