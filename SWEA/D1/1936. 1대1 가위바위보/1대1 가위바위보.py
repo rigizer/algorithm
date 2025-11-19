a46fb7816d240f32d3a25368ac84f852d85438c0
Jaeyong Han <rigizer@gmail.com>
2025-11-19 20:43:25 +0900

    [D1] Title: 1대1 가위바위보, Time: 51 ms, Memory: 50,048 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -1,16 +1,5 @@
 a, b = map(int, input().split())
-if a == 1:
-    if b == 2:
-        print('B')
-    elif b == 3:
-        print('A')
-elif a == 2:
-    if b == 1:
-        print('A')
-    elif b == 3:
-        print('B')
-elif a == 3:
-    if b == 1:
-        print('B')
-    elif b == 2:
-        print('A')
+if (a + 2) % 3 == b:
+    print('A')
+else:
+    print('B')
