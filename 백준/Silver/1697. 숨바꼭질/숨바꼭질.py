cd6c716e3987d477e59c5ac26e390d9c05264158
Jaeyong Han <rigizer@gmail.com>
2026-01-25 20:11:47 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 115712 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,26 +1,30 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
 from collections import deque
 
 def bfs(n, k):
     queue = deque()
     visited = [False for _ in range(100_001)]
-    
+
     queue.append([n, 0])
     visited[n] = True
-    
+
     while queue:
-        x, t = queue.popleft()
-        
+        node = queue.popleft()
+
+        x = node[0]
+        t = node[1]
+
         if x == k:
             return t
+        
+        for d in [-1, 1, x]:
+            nx = x + d
 
-        for nx in [x - 1, x + 1, 2 * x]:
-            if 0 <= nx <= 100_000 and not visited[nx]:
-                queue.append([nx, t + 1])
-                visited[nx] = True
+            if 0 <= nx <= 100_000:
+                if visited[nx] == False:
+                    queue.append([nx, t + 1])
+                    visited[nx] = True
 
 n, k = map(int, input().split())
 result = bfs(n, k)
+
 print(result)
